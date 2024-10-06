5cebbdcaea43e8b76c2ba8a59e0501820c5984ea
KRISHNADAS N <111467321+Krishnadas-N@users.noreply.github.com>
2024-10-06 13:32:11 +0530

    feat: add /keep-alive route to prevent app from sleeping

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ require('./config/passport')(passport);
 app.use('/admin', adminRouter);
 app.use('/', usersRouter);
 app.use('/cart',cartRouter)
-
+app.get('/keep-alive', (_req: Request, res: Response) => {
+    res.status(200).send('Server is alive!');
+})
 
 
 app.use(errorHandler);
